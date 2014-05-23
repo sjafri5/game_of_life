f2a4e4554a3a9484859b69eb37d9072c1890fa70
raza jafri <raza.jafri.sy@gmail.com>
2014-05-23 10:17:57 -0500

    added home page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GameOfLife::Application.config.secret_key_base = '7ae35e5572829f546257f859bdec6402537dc9105825306693005c503e6e5073767cd6845b0907985df5b5985f27aadcee36fa58e9a1226550180851358abaee'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GameOfLife::Application.config.session_store :cookie_store, key: '_game_of_life_session'
